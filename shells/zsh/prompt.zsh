f76a8b01f2c9d05e75af9180cd8fab7d8add000e
Simon <simonfong6@gmail.com>
2021-04-30 21:29:20 -0700

    Add ZSHRC shell

---
+++ shells/zsh/prompt.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+# https://medium.com/pareture/simplest-zsh-prompt-configs-for-git-branch-name-3d01602a6f33
+# Find and set branch name var if in git repository.
+function git_branch_name()
+{
+  branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+  if [[ $branch == "" ]];
+  then
+    :
+  else
+    echo '- ('$branch')'
+  fi
+}
+
+# Enable substitution in the prompt.
+setopt prompt_subst
+
+# Config for prompt. PS1 synonym.
+prompt='%2/ $(git_branch_name) > '
