1b9c1b6a9d4eee96f89d11970e4d50d3dbf48e4c
Simon <simonfong6@gmail.com>
2022-03-18 19:59:41 -0700

    g-cd

--- shells/zsh/git.zsh
+++ shells/zsh/git.zsh
@@ -13,3 +13,26 @@ g-crb() {
 
   git push --set-upstream origin $BRANCH
 }
+
+
+g-cd() {
+  REPOSITORY_NAME=$1
+  g-cd-host-user github.com simonfong6 $REPOSITORY_NAME
+}
+
+g-cd-host-user() {
+  GIT_HOST=$1
+  GITHUB_USER=$2
+  REPOSITORY_NAME=$3
+  REPO_ABSOLUTE_PATH="$HOME/git/$GIT_HOST/$GITHUB_USER/$REPOSITORY_NAME"
+
+  if [ -d "$REPO_ABSOLUTE_PATH" ]; then
+    echo "Repository $REPO_ABSOLUTE_PATH already cloned."
+  else
+    echo "Repository $REPO_ABSOLUTE_PATH does not exist, cloning..."
+    SSH_URL="git@$GIT_HOST:$GITHUB_USER/$REPOSITORY_NAME.git"
+    git clone $SSH_URL $REPO_ABSOLUTE_PATH
+  fi
+
+  cd $REPO_ABSOLUTE_PATH
+}
